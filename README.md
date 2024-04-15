7d78df4aeb02d01e8d34e33579a0ec9eb1cc4268
Hollow Man <hollowman@opensuse.org>
2024-04-15 22:51:05 +0300

    Use 1.3b model for faster inference
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 A RAG built with llama_index with lower level API
 
 [llama-2-13b-chat.Q4_0.gguf](https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main/llama-2-13b-chat.Q4_0.gguf)
+
+[f16-sheared-llama-1.3b.gguf](https://huggingface.co/Aryanne/Sheared-LLaMA-1.3B-gguf/resolve/main/f16-sheared-llama-1.3b.gguf)
--- components.py
+++ components.py
@@ -7,7 +7,7 @@ embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en", device="cuda"
 
 llm = LlamaCPP(
     # optionally, you can set the path to a pre-downloaded model instead of model_url
-    model_path="llama-2-13b-chat.Q4_0.gguf",
+    model_path="f16-sheared-llama-1.3b.gguf",
     temperature=0.1,
     max_new_tokens=256,
     # llama2 has a context window of 4096 tokens, but we set it lower to allow for some wiggle room
@@ -16,7 +16,7 @@ llm = LlamaCPP(
     generate_kwargs={},
     # kwargs to pass to __init__()
     # set to at least 1 to use GPU
-    model_kwargs={"n_gpu_layers": 1},
+    model_kwargs={"n_gpu_layers": 25},
     verbose=True,
 )
 
