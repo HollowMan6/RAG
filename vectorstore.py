b7c8acfba7d115cbcdeacbf30bb8697761e75f17
Hollow Man <hollowman@opensuse.org>
2024-04-16 21:46:20 +0300

    Add support for GPU acceleration
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- vectorstore.py
+++ vectorstore.py
@@ -7,8 +7,9 @@ from llama_index.core.vector_stores import (
 from llama_index.core.schema import BaseNode
 
 import numpy as np
-import jax.numpy as jnp
-from jax import jit, vmap
+import cupy as cp
+# import jax.numpy as jnp
+# from jax import jit, vmap
 
 from typing import Tuple, List, Any, Dict, cast
 from timeit import default_timer as timer
@@ -47,52 +48,84 @@ def get_top_k_embeddings(
     return result_similarities, result_ids
 
 
-@jit
-def get_top_k_jit(
-    query_embedding: List[float],
-    doc_embeddings: List[List[float]],
-):
-    """Get top nodes by similarity to the query."""
-    # Convert input lists to JAX arrays
-    # dimensions: D
-    qembed_jax = jnp.array(query_embedding)
-    # dimensions: N x D
-    dembed_jax = jnp.array(doc_embeddings)
-
-    # Define a function for computing cosine similarity
-    @vmap
-    def cosine_similarity(dembed):
-        # Compute dot product and norms
-        # dimensions: N
-        dproduct = jnp.dot(dembed, qembed_jax)
-        # dimensions: N
-        norm = jnp.linalg.norm(qembed_jax) * jnp.linalg.norm(dembed)
-        return dproduct / norm
-
-    # Vectorize the cosine_similarity function to handle multiple document embeddings
-    # Compute cosine similarities for all document embeddings
-    cos_sim_arr = cosine_similarity(dembed_jax)
-
-    # now we have the N cosine similarities for each document
-    # sort by cosine similarity
-    sorted_indices = jnp.argsort(cos_sim_arr)[::-1]
-
-    return sorted_indices, cos_sim_arr
-
-
-def get_top_k_embeddings_accelerated(
+def get_top_k_embeddings_gpu(
     query_embedding: List[float],
     doc_embeddings: List[List[float]],
     doc_ids: List[str],
     similarity_top_k: int = 5,
-) -> Tuple[List[float], List[str]]:
-    # Compile the function with JIT for performance optimization
+) -> Tuple[List[float], List]:
+    """Get top nodes by similarity to the query."""
+    # dimensions: D
+    qembed_cp = cp.array(query_embedding)
+    # dimensions: N x D
+    dembed_cp = cp.array(doc_embeddings)
+    # dimensions: N
+    dproduct_arr = cp.dot(dembed_cp, qembed_cp)
+    # dimensions: N
+    norm_arr = cp.linalg.norm(qembed_cp) * cp.linalg.norm(
+        dembed_cp, axis=1, keepdims=False
+    )
+    # dimensions: N
+    cos_sim_arr = dproduct_arr / norm_arr
 
-    sorted_indices, cos_sim_arr = get_top_k_jit(query_embedding, doc_embeddings)
-    top_k_indices = sorted_indices[:similarity_top_k]
-    result_ids = [doc_ids[i] for i in top_k_indices.tolist()]
+    # now we have the N cosine similarities for each document
+    # sort by top k cosine similarity, and return ids
+    tups = [(cos_sim_arr[i], doc_ids[i]) for i in range(len(doc_ids))]
+    sorted_tups = sorted(tups, key=lambda t: t[0], reverse=True)
 
-    return cos_sim_arr[top_k_indices].tolist(), result_ids
+    sorted_tups = sorted_tups[:similarity_top_k]
+
+    result_similarities = [s for s, _ in sorted_tups]
+    result_ids = [n for _, n in sorted_tups]
+    return result_similarities, result_ids
+
+
+# @jit
+# def get_top_k_jit(
+#     query_embedding: List[float],
+#     doc_embeddings: List[List[float]],
+# ):
+#     """Get top nodes by similarity to the query."""
+#     # Convert input lists to JAX arrays
+#     # dimensions: D
+#     qembed_jax = jnp.array(query_embedding)
+#     # dimensions: N x D
+#     dembed_jax = jnp.array(doc_embeddings)
+
+#     # Define a function for computing cosine similarity
+#     @vmap
+#     def cosine_similarity(dembed):
+#         # Compute dot product and norms
+#         # dimensions: N
+#         dproduct = jnp.dot(dembed, qembed_jax)
+#         # dimensions: N
+#         norm = jnp.linalg.norm(qembed_jax) * jnp.linalg.norm(dembed)
+#         return dproduct / norm
+
+#     # Vectorize the cosine_similarity function to handle multiple document embeddings
+#     # Compute cosine similarities for all document embeddings
+#     cos_sim_arr = cosine_similarity(dembed_jax)
+
+#     # now we have the N cosine similarities for each document
+#     # sort by cosine similarity
+#     sorted_indices = jnp.argsort(cos_sim_arr)[::-1]
+
+#     return sorted_indices, cos_sim_arr
+
+
+# def get_top_k_embeddings_accelerated(
+#     query_embedding: List[float],
+#     doc_embeddings: List[List[float]],
+#     doc_ids: List[str],
+#     similarity_top_k: int = 5,
+# ) -> Tuple[List[float], List[str]]:
+#     # Compile the function with JIT for performance optimization
+
+#     sorted_indices, cos_sim_arr = get_top_k_jit(query_embedding, doc_embeddings)
+#     top_k_indices = sorted_indices[:similarity_top_k]
+#     result_ids = [doc_ids[i] for i in top_k_indices.tolist()]
+
+#     return cos_sim_arr[top_k_indices].tolist(), result_ids
 
 
 def filter_nodes(nodes: List[BaseNode], filters: MetadataFilters):
@@ -125,16 +158,24 @@ def dense_search(query: VectorStoreQuery, nodes: List[BaseNode]):
     )
     end = timer()
     print(f"Execution time for normal one is {end - start} seconds")
+    # start = timer()
+    # result = get_top_k_embeddings_accelerated(
+    #     query_embedding,
+    #     doc_embeddings,
+    #     doc_ids,
+    #     similarity_top_k=query.similarity_top_k,
+    # )
+    # end = timer()
+    # print(f"Execution time for jax accelerated one is {end - start} seconds")
     start = timer()
-    result = get_top_k_embeddings_accelerated(
+    result = get_top_k_embeddings_gpu(
         query_embedding,
         doc_embeddings,
         doc_ids,
         similarity_top_k=query.similarity_top_k,
     )
     end = timer()
-    print(f"Execution time for jax accelerated one is {end - start} seconds")
-
+    print(f"Execution time for GPU accelerated one is {end - start} seconds")
     return result
 
 
